4484e71291b5987e2acc1aa77657ebf13c20d3b5
Kartik Fulara <kartikfulara87@gmail.com>
2022-12-19 14:57:58 +0530

    link error here

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
       <head />
+      <Link href="/upload">upload</Link>
       <body>{children}</body>
     </html>
-  )
+  );
 }
